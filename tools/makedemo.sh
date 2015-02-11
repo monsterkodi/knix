b19f35a8199b1026f47b8e553c6ae2d1288ccb65
monsterkodi <monsterkodi@gmx.net>
2015-02-11 19:02:19 +0100

    makedemo

--- tools/makedemo.sh
+++ tools/makedemo.sh
@@ -4,11 +4,13 @@ WEB=$(cd $(dirname "$0")/../web; pwd)
 
 cd $WEB
 echo `pwd`
-# echo 'copy json ...'
+
 rsync ../js/*.js js
 rsync ../js/lib/*.js js/lib
-# echo 'copy style ...'
 rsync ../style/dark.css style
 rsync ../style/bright.css style
-# echo 'copy index ...'
 rsync ../index.html demo.html
+
+git commit -am "latest demo"
+git push
+git status
